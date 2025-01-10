310e9131971ca9d7a669562d1c3d8fadae988e3d
Joy Sutrdhar <sutradharjoy2006@gmail.com>
2025-01-10 17:48:55 +0600

    Update MetaXsouL.py

--- MetaXsouL.py
+++ MetaXsouL.py
@@ -2,7 +2,7 @@ import google.generativeai as genai
 import streamlit as st
 genai.configure(api_key="AIzaSyBniEYo7qQCJdCkWfWHEdmE7s06Vg9XEmU")
 model = genai.GenerativeModel("gemini-1.5-flash")
-st.title("Generative AI")
+st.title("MetaXsouL AI")
 st.write("This is a simple demo of MetaXsouL AI. You can ask any question and get an answer from the AI model.")
 question = st.text_input("Enter your question here:")
 if st.button("Ask"):
