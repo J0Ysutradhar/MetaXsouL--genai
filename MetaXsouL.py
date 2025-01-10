8adf020f075fbf7d9142ec962bd294f7928345f8
Joy Sutrdhar <sutradharjoy2006@gmail.com>
2025-01-10 19:36:07 +0600

    Update MetaXsouL.py

--- MetaXsouL.py
+++ MetaXsouL.py
@@ -6,5 +6,9 @@ st.title("MetaXsouL AI")
 st.write("This is a simple demo of MetaXsouL AI. You can ask any question and get an answer from the AI model.")
 question = st.text_input("Enter your question here:")
 if st.button("Ask"):
-    response = model.generate_content(question)
-    st.write(response.text)
+    if "sutradhar" or "Sutradhar" in question:
+        st.write("He is my creator")
+    else:
+        response = model.generate_content(question)
+    
+        st.write(response.text)
